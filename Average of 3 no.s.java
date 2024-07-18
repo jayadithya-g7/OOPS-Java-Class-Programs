812530baa16060b5209db0efdee3a283cb8ec762
0xethjayadithya_g7/ <144100792+jayadithya-g7@users.noreply.github.com>
2024-07-18 11:06:26 +0530

    Update Average of 3 no.s.java (both user defined using scanner and predefining the values).java

--- Average of 3 no.s.java
+++ Average of 3 no.s.java
@@ -25,3 +25,26 @@ public class AverageCalculator {
         scanner.close();
     }
 }
+
+
+
+
+//or
+//--
+
+//without getting user inputs, if the inputs are predefined
+
+public class AverageCalculator {
+    public static void main(String[] args) {
+        // Define the three numbers
+        double number1 = 10.5;
+        double number2 = 20.5;
+        double number3 = 30.5;
+
+        // Calculate the average
+        double average = (number1 + number2 + number3) / 3;
+
+        // Print the average
+        System.out.println("The average of " + number1 + ", " + number2 + ", and " + number3 + " is: " + average);
+    }
+}
