10b5b2c1aaabf10f295903e7f4ca5023ace7314b
0xethjayadithya_g7/ <144100792+jayadithya-g7@users.noreply.github.com>
2024-07-22 14:38:49 +0530

    Update madhava series.java

--- madhava series.java
+++ madhava series.java
@@ -17,3 +17,28 @@ class HelloWorld {
         }
         p=p*4;
         System.out.printf("%f",p);
+
+
+    //or 
+
+    public class MadhavaSeriesPi {
+
+    public static void main(String[] args) {
+        int n = 20; // Number of terms to use in the series
+        double pi = calculatePi(n);
+        System.out.println("Approximation of pi using " + n + " terms: " + pi);
+    }
+
+    public static double calculatePi(int n) {
+        double pi = 0.0;
+        int sign = 1; // to alternate the sign of each term
+
+        for (int i = 0; i < n; ++i) {
+            int denominator = 2 * i + 1; // odd denominators: 1, 3, 5, 7, ...
+            pi += sign * 4.0 / denominator; // add or subtract term
+            sign = -sign; // alternate the sign for next term
+        }
+
+        return pi;
+    }
+}
