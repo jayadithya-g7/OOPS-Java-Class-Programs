1c14c0c128d80c8c309eb377e09d96bc02c9f92b
0xethjayadithya_g7dev/ <144100792+jayadithya-g7@users.noreply.github.com>
2024-09-17 17:18:56 +0530

    Create inheritancefunda.java

---
+++ OOPS/inheritancefunda.java
@@ -0,0 +1,40 @@
+//inheritance - passing one gen to other meaning
+
+//ek cls ka pty's given/passed to other cls
+
+import java.util.*;
+
+class Shape{
+
+    String color;
+    //diff shapes have different pty's, so let's create separate classes for them
+
+
+
+}
+
+class Triangle extends Shape{ //triangle cls ne extend/inherit karliya shape cls ko, automatically color pty comes now
+
+
+}
+
+public class Main2{
+    public static void main(String[] args) {
+
+        Triangle t1 = new Triangle();
+        t1.color = "red"; //so t1 has already ek color naam ka pty existing thi, and we assigned red
+        //inheritance is where one cls inheriting the ptys of other ptys
+
+        //reusability increases with subclses
+
+        //parent cls/base cls -from which cls we take ptys
+        //subcls - which cls takes ptys
+        
+        //in java 4 types of inheritance are there
+        
+
+
+
+        
+    }
+}
