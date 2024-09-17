65fa4dec36e7581d070cae917074c89d35762483
0xethjayadithya_g7dev/ <144100792+jayadithya-g7@users.noreply.github.com>
2024-09-17 12:43:13 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,49 @@
+
+class Main{
+    public static void main(String[] args) {
+
+        //string declaration
+        // String name = "jay";
+        // String fullName="jay adithya";
+        // String sentence ="jay adithya is a boy";
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.nextLine();
+        // System.out.println("Your name is: "+name);
+    
+
+        //concatenation
+        // String firstname ="jay";
+        // String lastname="adi";
+
+        // String fullname= firstname+" "+lastname;
+
+        // System.out.println(fullname);
+
+        // System.out.println(fullname.length()); //string fn length
+
+        //charAt method to access every character of a string
+
+        // for(int i=0;i<fullname.length();i++){
+        //     System.out.println(fullname.charAt(i));
+        // }
+
+        //comparison of 2 strings using compareTo
+
+        // if(firstname.compareTo(lastname)==0){
+        //     System.out.println("they are equal");
+
+        // }
+        // else{
+        //     System.out.println("unequal");
+
+
+        // String sentence ="my name is jay";
+        // //.substring(beg index, end index)
+
+        // String name = sentence.substring(11);
+        // System.out.println(name);
+
+
+        //note strings are immutable in java
+
+        
